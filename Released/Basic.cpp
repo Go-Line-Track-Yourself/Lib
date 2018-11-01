41d4cb86723d5675ddecaf938b2470071d406f7f
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2018-11-01 07:22:46 -0600

    sgn update

--- Released/Basic.cpp
+++ Released/Basic.cpp
@@ -2,7 +2,7 @@ double abs(double Var){
     if(Var<0)  Var=Var*(-1);
     return Var;
 }
-double sgn(double Var){
+int sgn(double Var){
     if(Var>0)   Var=1;
     else        Var=-1;
     return Var;
