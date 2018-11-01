bb40194b4b974cdda739946a7a0f5ca224a0b141
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2018-11-01 16:55:35 -0600

    Update Released/Basic.cpp

--- Released/Basic.cpp
+++ Released/Basic.cpp
@@ -1,8 +1,8 @@
-double abs(double Var){
+double ABS(double Var){
     if(Var<0)  Var=Var*(-1);
     return Var;
 }
-int sgn(double Var){
+int SGN(double Var){
     if(Var>0)   Var=1;
     else        Var=-1;
     return Var;
