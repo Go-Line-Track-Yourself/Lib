981aec9018ecf25ccef0e6ffad7c92aa3c5ce3e7
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2018-11-09 12:13:50 -0700

    Update Beta/Encoder_Calibration.cpp

--- Beta/Encoder_Calibration.cpp
+++ Beta/Encoder_Calibration.cpp
@@ -10,7 +10,7 @@ int JawCal(){
     int CalTimer=0;     //resets a local timer
     JawMotor.spin(vex::directionType::fwd, Rpm, vex::velocityUnits::rpm);   //starts the spin to hit the end stop 
     double MinChange=Rpm/240000*UpdateMsec;                                 ///(1/4)*(Rpm/60/1000); MinChange = 1/4 of requested rpm changed into msec instead of min
-    LastRotation=JawMotor.rotation(vex::rotationUnits::rev)+4*MinChange;    //makes sure that the while loop starts
+    int LastRotation=JawMotor.rotation(vex::rotationUnits::rev)+4*MinChange;    //makes sure that the while loop starts
     vex::task::sleep(100);                                                  //wait for the motor get some speed
     while(std::abs(JawMotor.rotation(vex::rotationUnits::rev)-LastRotation)>MinChange && CalTimer<TimeOut){//while the motors displacement is more then the MinChange and while the the timer is less then the timeout time
         LastRotation=JawMotor.rotation(vex::rotationUnits::rev);//update LastRotation
