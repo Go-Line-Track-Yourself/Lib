69e159be323e8eb58fa91810e9fc3d1deec12610
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2018-12-14 17:37:31 -0700

    initial -> begin

--- Beta/QuadRamp.cpp
+++ Beta/QuadRamp.cpp
@@ -1,8 +1,8 @@
 #include "QuadRamp.h"
 
 QuadRamp::QuadRamp(int dist, int begin, int max, int end) {
-   a = ((end + begin - 2*max) - 2*sqrt((end-max) * (initial-max))) / (dist * dist);
-   b = ((end-initial)/dist - a*dist) * SGN(dist);
+   a = ((end + begin - 2*max) - 2*sqrt((end-max) * (begin-max))) / (dist * dist);
+   b = ((end-begin)/dist - a*dist) * SGN(dist);
    c = start;
 }
 
