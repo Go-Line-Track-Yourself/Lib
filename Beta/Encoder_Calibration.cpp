5b3d4c0e323687b623da2a40d1c063c87ed744c0
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2019-01-15 14:33:27 -0700

    Update Encoder_Calibration.cpp

--- Beta/Encoder_Calibration.cpp
+++ Beta/Encoder_Calibration.cpp
@@ -1,28 +1,30 @@
-#ifndef Encoder_Calibration_cpp
-#define Encoder_Calibration_cpp
-//copy function and change motor object and change Rpm for direction
-//run this in a task so it doesnt stop the main cood loop
-#include <cmath>
-int JawCal(){
-    int Rpm=-200;       //Velocity to hit the end stop
-    int TimeOut=1000;   //Max time to hit end stop
-    int UpdateMsec=20;  //the time delay in the loop
-    int CalTimer=0;     //resets a local timer
-    JawMotor.spin(vex::directionType::fwd, Rpm, vex::velocityUnits::rpm);   //starts the spin to hit the end stop 
-    double MinChange=Rpm/240000*UpdateMsec;                                 ///(1/4)*(Rpm/60/1000); MinChange = 1/4 of requested rpm changed into msec instead of min
-    int LastRotation=JawMotor.rotation(vex::rotationUnits::rev)+4*MinChange;    //makes sure that the while loop starts
-    vex::task::sleep(100);                                                  //wait for the motor get some speed
-    while(std::abs(JawMotor.rotation(vex::rotationUnits::rev)-LastRotation)>MinChange && CalTimer<TimeOut){//while the motors displacement is more then the MinChange and while the the timer is less then the timeout time
-        LastRotation=JawMotor.rotation(vex::rotationUnits::rev);//update LastRotation
-        CalTimer=CalTimer+UpdateMsec;   //add time to the timer
-        vex::task::sleep(UpdateMsec);   //wait for the motor to spin
-    }                                   //motor stoped spinning or time ran out
-    if(CalTimer>=TimeOut) return 0;     //if timed out return error code 0 or false
-    else{                               //hit the end stop
-    JawMotor.resetRotation();           //reset the rotation
-    JawMotor.stop();                    //dont burn out the motor
-    return 1;                           //return 1 or true
+void LiftCal(){
+        LiftCaled=false;
+        int Rpm=-100;
+        int TimeOut=1000;
+        int UpdateMsec=50;
+        int CalTimer=0;
+        DI(Rpm);
+        double MinChange=Rpm/120000*UpdateMsec;///(1/4)*(Rpm/60/1000); MinChange = 1/4 of requested rpm but in msec`s
+        vex::task::sleep(250);//wait for acel
+        double LastRotation=LiftMotor.rotation(vex::rotationUnits::rev)/1000;//make sure it goes first loop
+        while(std::abs(LiftMotor.rotation(vex::rotationUnits::rev)-LastRotation)>MinChange && CalTimer<TimeOut){//if displacement is more then 1/4 of what it is set to be and it is not out of time
+            LastRotation=LiftMotor.rotation(vex::rotationUnits::rev);
+            CalTimer=CalTimer+UpdateMsec;
+            vex::task::sleep(UpdateMsec);
+        }
+        LiftMotor.resetRotation();
+        LiftMotor.stop();
+        LiftCaled=true;
+        LiftSpinToControlRunEnabled=true;
+        LiftSpinToControlEnabled=true;
+        while(LiftSpinToControlEnabled){
+            LiftSpinTo(LiftFlagPos[1]);
+            DR(LiftPctSetting);
+            EndTimeSlice();
+        }
     }
-}
-int jawcaltime=GlobTime
-#endif
+    int LiftCalTaskFun(){
+        LiftCal();
+        return 1;
+    }
