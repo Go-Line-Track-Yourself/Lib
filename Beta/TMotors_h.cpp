0ea8661bd5aab02b136e7d81dca46d778258d317
Gigantor2131 <44032190+Gigantor2131@users.noreply.github.com>
2019-01-22 15:59:13 -0700

    deNested

--- Beta/TMotors_h.cpp
+++ Beta/TMotors_h.cpp
@@ -26,18 +26,6 @@ class TMotors{
         double Rotation();
         void SetStop(vex::brakeType sunits);
 
-        class Controllers{
-            public:
-                bool SpinToControlEnabled=false;// currently spining to target
-                bool SpinToRunEnabled=false;// stops when it gets to target; gets rid of bounces
-                int SpinToVSetting=0;// requested dir; output
-                int Dir=1;// direction need to spin to get to target
-                bool MonoDir=false;// motor only spins one way to get to target
-                int LastTar=0;// last target->spinto
-
-                Controllers();
-                void SpinTo(int tar,bool sms,bool stop,bool rel,int v,int tal);
-        };
         // void SpinToInit(int Tar=0,bool SMS,bool Stop,int Rel);
         // void SpinTo(int Tar,int V,int Tal);
 };
